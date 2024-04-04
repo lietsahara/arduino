9457a2f7e37cb445f8a2728c4312f7e8e05fbac7
lietsahara <139396194+lietsahara@users.noreply.github.com>
2024-04-05 01:36:42 +0300

    Add files via upload
    my first arduino code. written and simulated at wowki.com

---
+++ potentiometer-controlled servo con display.ino
@@ -0,0 +1,42 @@
+/*for servo motor control*/
+#include <Servo.h>
+Servo servo1;//object creation
+
+int servopin=3;//servo signal pin connected to 3
+int potentiometer=A0;
+
+
+/*for output*/
+const int maxi=600;
+const int mins=0;
+void setup() {
+  //pinMode(A0, INPUT);
+Serial.begin(9600);
+/*servo*/
+servo1.attach(servopin);//connecting obj. to pin
+}
+void loop() {
+  /*reading printing*/
+int sensorreading=analogRead(A0);
+int range = map(sensorreading, mins, maxi,0, 3);
+switch(range){
+  case 0:
+    Serial.println("low");
+    break;
+  case 1:
+    Serial.println("med");
+    break;
+  case 2:
+    Serial.println("high");
+    break;
+  case 3:
+    Serial.println("higher");
+    break;
+}
+delay(3000);
+/*servo control*/
+int reading=analogRead(A0);
+int angle=map(reading, 0, 1023, 0, 180);
+
+servo1.write(angle);
+}
